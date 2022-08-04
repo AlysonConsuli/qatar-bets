6b7a428dbfe845b2bf37df9e70d1a3d68b0c5ec9
alyson <consulialyson@gmail.com>
2022-08-04 02:54:46 -0300

    feat: homepage

--- src/pages/Homepage/index.jsx
+++ src/pages/Homepage/index.jsx
@@ -1,12 +1,86 @@
-// import axios from "axios";
-// import { useContext, useState } from "react";
-// import { Link, useNavigate } from "react-router-dom";
-// import { Button } from "../../components/Button/index.jsx";
-// import { Input } from "../../components/Input/index.jsx";
-// import Swal from "sweetalert2";
-// import * as S from "../../styles/style.js";
-// import { UserContext } from "../../contexts/UserContext.jsx";
+/* eslint-disable react-hooks/exhaustive-deps */
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { UserContext } from "../../contexts/UserContext.jsx";
+import Modal from "react-modal";
+import * as S from "../../styles/style.js";
 
 export const Homepage = () => {
-  return <></>;
+  const navigate = useNavigate();
+  const { user, setUser } = useContext(UserContext);
+  const [logoutModal, setLogoutModal] = useState(false);
+  Modal.setAppElement(document.querySelector(".root"));
+
+  function logout() {
+    setUser(null);
+    localStorage.removeItem("users");
+    navigate("/");
+  }
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/");
+    }
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${user?.token}`,
+      },
+    };
+
+    axios
+      .post(`${process.env.REACT_APP_API_URL}/auto-login`, {}, config)
+      .then(() => {})
+      .catch(() => {
+        logout();
+      });
+  }, []);
+
+  return (
+    <>
+      <Modal
+        isOpen={logoutModal}
+        onRequestClose={() => setLogoutModal(false)}
+        className="_"
+        overlayClassName="_"
+        contentElement={(props, children) => (
+          <S.ModalStyle {...props}>{children}</S.ModalStyle>
+        )}
+        overlayElement={(props, contentElement) => (
+          <S.OverlayStyle {...props}>{contentElement}</S.OverlayStyle>
+        )}
+      >
+        <span>
+          Are you sure you want <br /> to logout?
+        </span>
+        <div>
+          <button onClick={() => setLogoutModal(false)}>No, go back</button>
+          <button onClick={logout}>Yes, logout</button>
+        </div>
+      </Modal>
+      <S.PageContainer>
+        <S.Header>
+          <h1>Qatar Bets</h1>
+          <S.LogoutIcon onClick={() => setLogoutModal(true)}></S.LogoutIcon>
+        </S.Header>
+        <S.ArticleContainer>
+          <Link to="/add-bets">
+            <S.Article>
+              <span>Add bets</span>
+            </S.Article>
+          </Link>
+          <S.Article>
+            <span>Show bets</span>
+          </S.Article>
+          <S.Article>
+            <span>Ranking</span>
+          </S.Article>
+          <S.Article>
+            <span>Games</span>
+          </S.Article>
+        </S.ArticleContainer>
+      </S.PageContainer>
+    </>
+  );
 };
--- src/pages/Homepage/style.js
+++ src/pages/Homepage/style.js
@@ -1,6 +1,56 @@
 import styled from "styled-components";
 
-export const HomeContainer = styled.main`
+import { FiLogOut } from "react-icons/fi";
+
+export const PageContainer = styled.main`
   background-color: ${({ theme }) => theme.colors.mainBackground};
   height: 100vh;
+`;
+
+export const Header = styled.header`
+  position: sticky;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 72px;
+  background-color: ${({ theme }) => theme.colors.secondaryBackground};
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  h1 {
+    padding-left: 20px;
+    font-size: 28px;
+    color: ${({ theme }) => theme.colors.text};
+  }
+  svg {
+    margin-right: 20px;
+    color: ${({ theme }) => theme.colors.text};
+  }
+`;
+
+export const LogoutIcon = styled(FiLogOut)`
+  font-size: 25px;
+  cursor: pointer;
+`;
+
+export const ArticleContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 40px;
+  margin-top: 30px;
+`;
+
+export const Article = styled.article`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100px;
+  height: 100px;
+  background-color: ${({ theme }) => theme.colors.secondary};
+  border-radius: 50%;
+  cursor: pointer;
+  span {
+    font-size: 20px;
+    color: ${({ theme }) => theme.colors.secondaryBackground};
+  }
 `;
