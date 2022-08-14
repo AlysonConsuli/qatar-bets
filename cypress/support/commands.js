fddf90d93e33a16ef73cff6a729781f27c83c52e
alyson <consulialyson@gmail.com>
2022-08-14 19:40:28 -0300

    feat: cypress enter in the add-bets pages from homepage

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -18,3 +18,24 @@ Cypress.Commands.add("resetAllData", () => {
 Cypress.Commands.add("createUser", (user) => {
   cy.request("POST", `${BACKEND_URL}/sign-up`, user);
 });
+
+Cypress.Commands.add("createUserAndLogin", (user) => {
+  cy.request("POST", `${BACKEND_URL}/sign-up`, user).then(() => {
+    cy.request({
+      method: "POST",
+      url: `${BACKEND_URL}/sign-in`,
+      body: user,
+    }).then((resp) => {
+      const userSerialized = JSON.stringify(resp.body);
+      window.localStorage.setItem("user", userSerialized);
+    });
+  });
+});
+
+Cypress.Commands.add("createGame", (user) => {
+  cy.request({
+    method: "POST",
+    url: `${BACKEND_URL}/create-game`,
+    headers,
+  });
+});
